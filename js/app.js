751151f1a827a5f093df68b52362985a48a054a8
arbinzaman <arbinzmn@gmail.com>
2022-09-02 17:48:26 +0600

    finised project

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const displayMeals = meals =>{
     // console.log(meal);
     mealDiv.classList.add('col');
     mealDiv.innerHTML= `
-        <div class="card">
+        <div onclick ="displayMealDeatils(${meal.idMeal})"class="card">
                 <img class="card-img-top" src="${meal.strMealThumb}" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -30,4 +30,27 @@ const searchMeals =()=>{
    loadMeals(searchText);
    searchField.value='';
 }
-loadMeals();
+
+const displayMealDeatils = (idMeal)=>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch (url)
+    .then(res => res.json())
+    .then(data => displayFoodDeatils(data.meals[0]))
+}
+
+const displayFoodDeatils = meal => {
+    const detailsContainer = document.getElementById('detail-Container');
+    detailsContainer.innerHTML='';
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add ('card');
+    mealDiv.innerHTML = `
+             <img class="card-img" src="${meal.strMealThumb}" alt="Card image">
+                <div class="card-img-overlay">
+                  <h5 class="card-title">${meal.strMeal}</h5>
+                  <p class="card-text">${meal.strInstructions.s}</p>
+              </div>
+    
+    `;
+    detailsContainer.appendChild(mealDiv);
+}
+loadMeals('');
